7ffbdef6a69e860fd10b213000cab60c5834b1fe
sleeplessai <2018063+sleeplessai@users.noreply.github.com>
2021-11-03 00:49:47 +0800

    first commit

---
+++ train.sh
@@ -0,0 +1,5 @@
+python train.py \
+  --root_dir /mnt/sda1/dataset3d/DTU/mvs_training/dtu \
+  --num_epochs 6 --batch_size 1 \
+  --n_depths 192 --interval_scale 1.06 \
+  --optimizer adam --lr 1e-3 --lr_scheduler cosine
